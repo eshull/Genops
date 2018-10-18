72a5e3e129b770b680e7c263d66d4693f0c0bacd
Eric Shull <eshulldesign@gmail.com>
2018-10-18 13:33:17 -0700

    positive fail for has and belongs to many system nodes

--- app/models/system_node.rb
+++ app/models/system_node.rb
@@ -1,4 +1,18 @@
 class SystemNode < ApplicationRecord
   has_many :configurations
-  has_many :system_nodes, through: :system_links
+
+  has_many :from_node_links, :foreign_key => :from_node_id,
+          :class_name => 'SystemLink'
+  has_many :from_nodes, :through => :from_node_links,
+          :source => :to_node
+
+  has_many :to_node_links, :foreign_key => :to_node_id,
+           :class_name => 'SystemLink'
+  has_many :to_nodes, :through => :to_node_links,
+           :source => :from_node
+
+  def links
+    (from_node_links + to_node_links).flatten.uniq
+  end
+  # Call @system_node.links to see all the associated links
 end
---
+++ spec/models/system_node_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe SystemNode do
+  it { have_and_belong_to_many :system_node }
+end
