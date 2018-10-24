3d7ee0a0c1e43182a2ac0fbd0f3f254c49caa88f
Eric Shull <eshulldesign@gmail.com>
2018-10-24 15:20:40 -0700

    add seeds file and rails routes for settings belonging to system nodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SystemNode.destroy_all
+Setting.destroy_all
+
+SystemNode.create!(name: 'Reporter', address: 'reporter address example', description: 'reporter description example')
+
+SystemNode.create!(name: 'Reviewer', address: 'reviewer address example', description: 'reviewer description example')
+
+SystemNode.create!(name: 'Cerner', address: 'cerner address example', description: 'cerner description example')
